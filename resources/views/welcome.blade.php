2d11114b7ed09427831cf8590e62fef45baedecf
septiandrd <septiandrd@gmail.com>
2017-11-04 20:06:17 +0700

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,386 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <title>SMAN 1 Margahayu</title>
+    <!-- BOOTSTRAP CORE STYLE CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet"/>
+    <!-- FONT AWESOME CSS -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet"/>
+    <!-- FLEXSLIDER CSS -->
+    <link href="assets/css/flexslider.css" rel="stylesheet"/>
+    <!-- CUSTOM STYLE CSS -->
+    <link href="assets/css/style.css" rel="stylesheet"/>
+    <!-- Google	Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'/>
+</head>
+
+<body>
+
+    {{--NAVBAR SECTION START--}}
+    <div class="navbar navbar-inverse navbar-fixed-top " id="menu">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><img class="logo-custom" src="assets/img/logo.jpg" alt=""/></a>
+            </div>
+            <div class="navbar-collapse collapse move-me">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#home">HOME</a></li>
+                    <li><a href="#stop1">PROFIL</a></li>
+                    <li><a href="#stop3">FASILITAS</a></li>
+                    <li><a href="#course-sec">INFO & BERITA</a></li>
+                    <li><a href="#contact-sec">GALERI</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    {{--NAVBAR SECTION END--}}
+
+    {{--SLIDER SECTION START--}}
+    <div class="home-sec" id="home">
+        <div class="overlay">
+            <div class="container">
+                <div class="row text-center-up ">
+                    <div class="col-lg-12  col-md-12 col-sm-12">
+                        <div class="flexslider set-flexi" id="main-section">
+                            <ul class="slides move-me">
+                                <!-- Slider 01 -->
+                                <li>
+                                    <h3>Selamat Datang di Website</h3>
+                                    <h1>SMAN 1 MARGAHAYU</h1>
+                                    <a href="#stop1" class="btn btn-info btn-lg" style="width: 130px">
+                                        PROFIL
+                                    </a>
+                                    <a href="#stop2" class="btn btn-info btn-lg" style="width: 130px">
+                                        VISI MISI
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{--SLIDER SECTION END--}}
+
+    {{--STOP MARK 1 START--}}
+    <div id="stop1" class="tag-line">
+        <div class="container">
+            <div class="row  text-center">
+                <div class="col-lg-12  col-md-12 col-sm-12">
+                    <h2 data-scroll-reveal="enter from the bottom after 0.1s"></h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{--STOP MARK 1 END--}}
+
+    {{--PROFIL SEKOLAH START--}}
+    <div class="container set-pad">
+        <div class="row text-center">
+            <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+                <h1 data-scroll-reveal="enter from the bottom after 0.2s" class="header-line">PROFIL SEKOLAH</h1>
+                <p data-scroll-reveal="enter from the bottom after 0.3s">
+                    SMA Negeri 1 Margahayu, merupakan salah satu Sekolah Menengah Atas Negeri yang berlokasi
+                    di Kabupaten Bandung, Provinsi Jawa Barat, Indonesia. Sama dengan SMA pada umumnya di Indonesia,
+                    masa pendidikan sekolah di SMAN 1 Margahayu ditempuh dalam waktu tiga tahun pelajaran, mulai dari
+                    Kelas X sampai Kelas XII.
+                </p>
+                <hr>
+                <h3><b>KEPALA SEKOLAH</b></h3>
+                <br>
+                <img src="assets/img/kepsek.jpg" class="img-rounded" style="width: 350px;padding: 5px;border: double"
+                     data-scroll-reveal="enter from the bottom after 0.3s">
+                <br>
+                <h4><b>Drs. H. Aa Sudaya, M.Pd</b></h4>
+                <br>
+                <hr>
+                <h3><b>ALAMAT</b></h3>
+                <p data-scroll-reveal="enter from the bottom after 0.3s">
+                    Jl. KH. Wahid Hasyim No. 387, Kecamatan Margahayu, Kabupaten Bandung 40226, Jawa Barat, Indonesia
+                    <b>Telp./Faks. </b>022-5405962 <br>
+                    <b>Email : </b>sman_1_margahayu@yahoo.co.id</p>
+            </div>
+        </div>
+    </div>
+    {{--PROFIL SEKOLAH END--}}
+
+    {{--STOP MARK 2 START--}}
+    <div id="stop2" class="tag-line">
+        <div class="container">
+            <div class="row  text-center">
+                <div class="col-lg-12  col-md-12 col-sm-12">
+                    <h2 data-scroll-reveal="enter from the bottom after 0.1s"></h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{--STOP MARK 2 END--}}
+
+    {{--VISI MISI START--}}
+    <div id="faculty-sec">
+        <div class="container set-pad">
+            <div class="row text-center">
+                <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+                    <h1 data-scroll-reveal="enter from the bottom after 0.1s" class="header-line">VISI & MISI SEKOLAH</h1>
+                    <h3><b>VISI</b></h3>
+                    <h4 data-scroll-reveal="enter from the bottom after 0.3s">Terwujudnya lulusan yang unggul dalam mutu,
+                        berwawasan global, cinta lingkungan berlandaskan
+                        Iman dan Taqwa
+                    </h4>
+
+                </div>
+
+            </div>
+            <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+                <h3 class="text-center"><b>MISI</b></h3>
+                <ul data-scroll-reveal="enter from the bottom after 0.3s">
+                    <h4>
+                        <li>Melaksanakan pemenuhan delapan standar Nasional Pendidikan</li>
+                    </h4>
+                    <h4>
+                        <li>Meningkatkan prestasi akademik dan non akademik siswa</li>
+                    </h4>
+                    <h4>
+                        <li>Meningkatkan kompetensi pendidik dan tenaga kependidikan</li>
+                    </h4>
+                    <h4>
+                        <li>Melaksanakan pengembangan kurikulum</li>
+                    </h4>
+                    <h4>
+                        <li>Memanfaatkan sarana pembelajaran sebagai Pusat Sumber Belajar</li>
+                    </h4>
+                    <h4>
+                        <li>Memanfaatkan perkembangan IPTEK untuk membantu kegiatan pembelajaran</li>
+                    </h4>
+                    <h4>
+                        <li>Melaksanakan kegiatan pembelajaran berbasis keunggulan lokal</li>
+                    </h4>
+                    <h4>
+                        <li>Meningkatkan pemahaman tentang Budaya Lokal</li>
+                    </h4>
+                    <h4>
+                        <li>Mengembangkan kegiatan pembelajaran berbasis lingkungan</li>
+                    </h4>
+                    <h4>
+                        <li>Melaksanakan kegiatan pengelolaan lingkungan hidup</li>
+                    </h4>
+                    <h4>
+                        <li>Menciptakan suasana lingkungan pembelajaran yang nyaman</li>
+                    </h4>
+                    <h4>
+                        <li>Mengembangkan nilai-nilai agama dan budaya dalam kegiatan pembelajaran</li>
+                    </h4>
+                    <h4>
+                        <li>Melaksanakan kegiatan peringatan hari-hari besar keagamaan</li>
+                    </h4>
+                    <h4>
+                        <li>Mengembangkan kegiatan peningkatan iman dan taqwa.</li>
+                    </h4>
+                </ul>
+                <br><br id="stop3">
+            </div>
+        </div>
+    </div>
+    {{--VISI MISI END--}}
+
+    {{--STOP MARK 3 START--}}
+
+    {{--STOP MARK 3 END--}}
+
+    {{--FASILITAS START--}}
+    <div id="course-sec" class="container set-pad">
+        <div class="row text-center">
+            <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+                <h1 data-scroll-reveal="enter from the bottom after 0.1s" class="header-line">FASILITAS</h1>
+                <p data-scroll-reveal="enter from the bottom after 0.3s">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                    Aenean commodo.
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                    Aenean commodo.
+                </p>
+            </div>
+
+        </div>
+        <!--/.HEADER LINE END-->
+
+
+        <div class="row set-row-pad">
+            <div class="col-lg-6 col-md-6 col-sm-6 " data-scroll-reveal="enter from the bottom after 0.4s">
+                <img src="assets/img/building.jpg" class="img-thumbnail"/>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-lg-offset-1 col-md-offset-1 col-sm-offset-1">
+                <div class="panel-group" id="accordion">
+                    <div class="panel panel-default" data-scroll-reveal="enter from the bottom after 0.5s">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse1" class="collapsed">
+                                    <strong> 350+</strong> DESIGNING COURSES
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapse1" class="panel-collapse collapse" style="height: 0px;">
+                            <div class="panel-body">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sagittis egestas mauris
+                                    ut vehicula. Cras viverra ac orci ac aliquam. Nulla eget condimentum mauris, eget
+                                    tincidunt est.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-default" data-scroll-reveal="enter from the bottom after 0.7s">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse2" class="collapsed">
+                                    <strong> 250+</strong> ENGINEERING COURSES
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapse2" class="panel-collapse collapse" style="height: 0px;">
+                            <div class="panel-body">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sagittis egestas mauris
+                                    ut vehicula. Cras viverra ac orci ac aliquam. Nulla eget condimentum mauris, eget
+                                    tincidunt est.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-default" data-scroll-reveal="enter from the bottom after 0.9s">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse3" class="collapsed">
+                                    <strong> 153+</strong> MANAGEMENT COURSES
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapse3" class="panel-collapse collapse" style="height: 0px;">
+                            <div class="panel-body">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sagittis egestas mauris
+                                    ut vehicula. Cras viverra ac orci ac aliquam. Nulla eget condimentum mauris, eget
+                                    tincidunt est.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="alert alert-info" data-scroll-reveal="enter from the bottom after 1.1s">
+                           <span style="font-size:40px;">
+                              <strong> 2500 + </strong>
+                               <hr/>
+                               Centers
+                           </span>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+    {{--FASILITAS END--}}
+
+    {{--CONTACT US START--}}
+    <div id="contact-sec">
+        <div class="overlay">
+            <div class="container set-pad">
+                <div class="row text-center">
+                    <div class="col-lg-8 col-lg-offset-2 col-md-8 col-sm-8 col-md-offset-2 col-sm-offset-2">
+                        <h1 data-scroll-reveal="enter from the bottom after 0.1s" class="header-line">CONTACT US </h1>
+                        <p data-scroll-reveal="enter from the bottom after 0.3s">
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                            Aenean commodo.
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                            Aenean commodo.
+                        </p>
+                    </div>
+
+                </div>
+                <!--/.HEADER LINE END-->
+                <div class="row set-row-pad" data-scroll-reveal="enter from the bottom after 0.5s">
+
+
+                    <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
+                        <form>
+                            <div class="form-group">
+                                <input type="text" class="form-control " required="required" placeholder="Your Name"/>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control " required="required" placeholder="Your Email"/>
+                            </div>
+                            <div class="form-group">
+                                <textarea name="message" required="required" class="form-control" style="min-height: 150px;"
+                                          placeholder="Message"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-info btn-block btn-lg">SUBMIT REQUEST</button>
+                            </div>
+
+                        </form>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+    {{--CONTACT US END--}}
+
+    {{--LOCATION START--}}
+    <div class="container">
+        <div class="row set-row-pad">
+            <div class="col-lg-4 col-md-4 col-sm-4   col-lg-offset-1 col-md-offset-1 col-sm-offset-1 "
+                 data-scroll-reveal="enter from the bottom after 0.4s">
+
+                <h2><strong>Our Location </strong></h2>
+                <hr/>
+                <h4>234/80 -UFG , New Street,</h4>
+                <h4>Switzerland.</h4>
+                <h4><strong>Call:</strong> + 67-098-907-1269 / 70 / 71 </h4>
+                <h4><strong>Email: </strong>info@yourdomain.com</h4>
+            </div>
+
+
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4   col-lg-offset-1 col-md-offset-1 col-sm-offset-1"
+             data-scroll-reveal="enter from the bottom after 0.4s">
+
+            <h2><strong>Social Conectivity </strong></h2>
+            <hr/>
+            <div>
+                <a href="#"> <img src="assets/img/Social/facebook.png" alt=""/> </a>
+                <a href="#"> <img src="assets/img/Social/google-plus.png" alt=""/></a>
+                <a href="#"> <img src="assets/img/Social/twitter.png" alt=""/></a>
+            </div>
+        </div>
+    </div>
+    {{--LOCATION END--}}
+
+    <div id="footer">
+        &copy 2014 yourdomain.com | All Rights Reserved | <a href="http://binarytheme.com" style="color: #fff"
+                                                             target="_blank">Design by : binarytheme.com</a>
+    </div>
+    <!-- FOOTER SECTION END-->
+
+    <!--  Jquery Core Script -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!--  Core Bootstrap Script -->
+    <script src="assets/js/bootstrap.js"></script>
+    <!--  Flexslider Scripts -->
+    <script src="assets/js/jquery.flexslider.js"></script>
+    <!--  Scrolling Reveal Script -->
+    <script src="assets/js/scrollReveal.js"></script>
+    <!--  Scroll Scripts -->
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <!--  Custom Scripts -->
+    <script src="assets/js/custom.js"></script>
+</body>
+</html>
